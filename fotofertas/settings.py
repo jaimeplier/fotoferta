a3dad79f6e228162bb9791ba132f63a601d21eb7
salvador <salvadorjnt@gmail.com>
2019-07-02 18:45:44 -0500

    Creación de login para administrador

--- fotofertas/settings.py
+++ fotofertas/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
 
+AUTH_USER_MODEL = 'config.Usuario'
 
 # Application definition
 
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login as auth_login, logout
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
-# Create your views here.
+
+def login(request):
+    error_message = ''
+    if request.user.is_authenticated:
+        return redirect(reverse('webapp:list_chofer'))
+    if request.method == 'POST':
+        correo = request.POST['correo']
+        password = request.POST['password']
+        user = authenticate(correo=correo, password=password)
+        if user is not None:
+            if user.estatus:
+                auth_login(request, user)
+                if request.POST.get('next') is not None:
+                    return redirect(request.POST.get('next'))
+                elif user.rol.pk == 2: # Administrador Fotofertas
+                    return redirect(reverse('administrador:list_marco'))
+                # elif user.rol.pk in [3,4]: # Cliente o Fotopartner
+                #     return redirect(reverse('webapp:'))
+                logout(request)
+                return redirect(reverse('webapp:login_unauthorized'))
+            else:
+                error_message = "Usuario inactivo"
+        else:
+            error_message = "Usuario y/o contraseña incorrectos"
+    context = {
+        'error_message': error_message
+    }
+    if request.GET.get('next') is not None:
+        context['next'] = request.GET.get('next')
+    return render(request, 'config/login.html', context)
