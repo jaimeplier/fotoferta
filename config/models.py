dc8ab7ae7d4116ddd01d7d048dd3dd44e5345a2e
salvador <salvadorjnt@gmail.com>
2019-07-11 11:04:45 -0500

    Creación de modelos PapelImpresion y TipoPapel

--- config/models.py
+++ config/models.py
@@ -408,23 +408,24 @@ class MariaLuisa(Catalogo):
         db_table = 'maria_luisa'
 
 
-# class GrosorPapel(models.Model):
-#     medida = models.FloatField()
-#
-#     class Meta:
-#         managed = True
-#         db_table = 'grosor_papel'
-#
-#     def __str__(self):
-#         return str(self.medida) + ' mm.'
-#
-# class TipoPapel(Catalogo):
-#     grosor = models.ForeignKey('GrosorPapel', models.DO_NOTHING)
-#     precio = models.FloatField()
-#
-#     class Meta:
-#         managed = True
-#         db_table = 'tipo_papel'
+class TipoPapel(Catalogo):
+
+    class Meta:
+        managed = True
+        db_table = 'tipo_papel'
+
+class PapelImpresion(Catalogo):
+    tipo_papel = models.ForeignKey('TipoPapel', models.DO_NOTHING)
+    tamanio = models.ForeignKey('Tamanio', models.DO_NOTHING)
+    precio = models.FloatField()
+
+    class Meta:
+        managed = True
+        db_table = 'papel_impresion'
+
+
+    def __str__(self):
+        return self.tipo_papel.nombre + ' ' + self.tamanio.nombre
 
 class Textura(Catalogo):
     imagen = models.ImageField(upload_to='img_texturas/',
@@ -515,7 +516,7 @@ class Producto(models.Model):
     marco = models.ForeignKey(Marco, models.DO_NOTHING, null=True, blank=True)
     maria_luisa = models.ForeignKey(MariaLuisa, models.DO_NOTHING, null=True, blank=True)
     tipo_compra = models.ForeignKey(TipoCompra, models.DO_NOTHING)
-    # tipo_papel = models.ForeignKey('TipoPapel', models.DO_NOTHING, null=True, blank=True)
+    papel_impresion = models.ForeignKey('PapelImpresion', models.DO_NOTHING, null=True, blank=True)
     promocion_aplicada = models.ForeignKey('Promocion', models.DO_NOTHING, null=True, blank=True)
     estatus_pago_fotografo = models.ForeignKey(EstatusPago, models.DO_NOTHING)
 
