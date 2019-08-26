51a94e0325b7b1b5df9e9d14d70c15363c86f75c
salvador <salvadorjnt@gmail.com>
2019-08-26 18:07:00 -0500

    modificacion de modelo Fotografia

--- config/models.py
+++ config/models.py
@@ -304,6 +304,7 @@ class Fotografia(models.Model):
     publica = models.BooleanField(default=True) # Si la foto se mostrara en la red social
     aprobada = models.BooleanField(default=False) # Si fue aprobada por un administrador
     estatus = models.BooleanField(default=True) # Si la imagen debe mostrarse o no
+    gratuita = models.BooleanField(default=False) # Si la fotografia será gratuita
     fecha_alta = models.DateTimeField(auto_now_add=True)
     likes = models.BigIntegerField(default=0)
 
