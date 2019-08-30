25c60cb07f38b5395367d59eaa4d311df4bce4df
salvador <salvadorjnt@gmail.com>
2019-08-29 19:37:59 -0500

    envio de correos de confirmacion de pago con web hook para descarga de productos

--- webservices/webhook.py
+++ webservices/webhook.py
@@ -1,11 +1,16 @@
+import jwt
 from django.http import HttpResponse
+from django.template.loader import get_template
+from django.utils import timezone
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.parsers import JSONParser
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from config.models import Orden, EstatusPago
+from config.models import Orden, EstatusPago, Producto, Descarga
+from fotofertas.settings import KEY_FOTO
+from webapp.mail import sendMail
 from webservices.Permissions import WebHookPermission
 
 
@@ -19,9 +24,35 @@ class WebHook(APIView):
         if type == 'order.paid':
             id = request.data['data']['object']['order_id']
             try:
-                p = Orden.objects.get(order_id=id)
-                p.estatus_pago = EstatusPago(pk=2)
-                p.save()
+                orden = Orden.objects.get(order_id=id)
+                estatus_pago = EstatusPago.objects.get(pk=2)  # Pagado
+                orden.estatus_pago = estatus_pago
+                orden.fecha_compra = timezone.now()
+                orden.save()
+                productos = Producto.objects.filter(orden=orden)
+                productos.update(estatus_pago=estatus_pago)
+                productos.filter(subtotal__gt=0)
+                descargas = []
+                for producto in productos:
+                    encoded = jwt.encode(
+                        {'producto': producto.pk, 'foto': producto.foto.pk, 'usuario': self.request.user.pk}, KEY_FOTO,
+                        algorithm='HS256')
+                    token = encoded.decode('UTF-8')
+                    descargas.append(
+                        Descarga.objects.create(producto=producto, orden=orden, usuario=self.request.user, token=token))
+
+                email_template_name = 'mailing/descargas.html'
+                subject = "Productos Digitales"
+                to = [self.request.user.correo]
+                ctx = {
+                    'productos': descargas,
+                    'request': request,
+                    'user': self.request.user,
+                    'orden': orden
+                }
+                message = get_template(email_template_name).render(ctx)
+                sendMail(to, subject, message)
+
             except Orden.DoesNotExist:
                 pass
         obj = request.data['object']
